394fe0a0db4b8ff40c77591ab099b6434f4a492a
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2024-07-13 11:25:18 -0400

    Scroll when too big

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import './App.css'
-import ToDoContainer from './components/ToDoContainer';
+import "./App.css";
+import ToDoContainer from "./components/ToDoContainer";
 
 function App() {
-
   return (
-    <div className="flex justify-center h-screen items-center bg-slate-950 text-slate-100">
-      <ToDoContainer/>
+    <div className="flex justify-center h-screen items-center overflow-y-auto bg-slate-950 text-slate-100">
+      <ToDoContainer />
     </div>
   );
 }
 
-export default App
+export default App;
